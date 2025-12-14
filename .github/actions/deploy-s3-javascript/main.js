e1023ba5db99f7e190d03b0f23a99094efc9d2ff
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-12-14 18:44:50 +0000

    Fix. invalid constant names for job inputs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,8 +4,8 @@ const exec = require('@actions/exec');
 
 function run() {
   // 1) Get some input values
-  const bucket = core.getInput('bucket', { required: true });
-  const region = core.getInput('region', { required: true });
+  const bucket = core.getInput('bucket-name', { required: true });
+  const region = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
 
   // 2) Upload files to S3
