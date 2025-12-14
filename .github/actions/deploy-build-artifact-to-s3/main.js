6ec09f98d2b431a0417d242ef2e76ae5e2ceb3bd
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-12-14 18:25:46 +0000

    Add. javascript custom action

--- .github/actions/deploy-build-artifact-to-s3/main.js
+++ .github/actions/deploy-build-artifact-to-s3/main.js
@@ -1,9 +1,18 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+const core = require('@actions/core');
+// const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
-   core.notice("Hello this is a message from custom javascript action!")
+  // 1) Get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files to S3
+  const s3URI = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${region}`);
+
+  core.notice("Hello this is a message from custom javascript action!");
 }
 
 run();
